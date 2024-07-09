d44d39c24f4ab32c9ba9a362b3be37513dc101d6
B4IGit <dfledesma@dmacc.edu>
2024-07-09 14:26:31 -0500

    Setting classes and text condotionally

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -110,12 +110,12 @@ function Menu() {
 function Pizza({pizzaObj}) {
     console.log(pizzaObj)
     return (
-        <li className="pizza">
+        <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ""}`}>
             <img src={pizzaObj.photoName} alt={pizzaObj.name}/>
             <div>
                 <h3>{pizzaObj.name}</h3>
                 <p>{pizzaObj.ingredients}</p>
-                <span>{pizzaObj.price + 3}</span>
+                <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price + 3}</span>
             </div>
         </li>
     );
